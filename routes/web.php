9e3d59574009a1752ae873252b392e2b76f44d65
AfrahOrhyza <aorhyza@gmail.com>
2026-01-27 13:26:31 +0700

    update project

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,20 @@
 
 use Illuminate\Support\Facades\Route;
 
+// ROOT tetap ke login (untuk first time user)
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
+});
+
+// Tapi buat akses cepat ke dashboard (buat development)
+Route::get('/dashboard', function () {
+    return view('dashboard.index');
+});
+
+Route::get('/login', function () {
+    return view('auth.login');
+});
+
+Route::get('/register', function () {
+    return view('auth.register');
 });
